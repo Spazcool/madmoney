6c8bae41e21b3eb691f2c72d9eabcae94752f459
Spazcool <douglas.wrightiii@gmail.com>
2018-05-28 14:50:21 +0800

    pc dying

--- src/Tools/Calcul.js
+++ src/Tools/Calcul.js
@@ -6,123 +6,140 @@ class Calcul extends Component {
   constructor() {
     super();
     this.state = {
-      variable_A: '',
-      variable_B: '',
-      result: 0,
+      basePrice:'',
+      lostMonthes:'',
+      monthlyExpenses:'',
+      monthlyMortgage:'',
+      monthlyRent:'',
+      repairCosts:'',
+      brut:0,
+      net:0,
+      cashFlow:0,
+      data:[],
     };
-    this.addition = this.addition.bind(this);
+    this.calculateYield = this.calculateYield.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
-  addition(A,B) {
-    let _result = parseInt(A, 10) + parseInt(B, 10);
-    this.setState({
-      result: _result,
-    });
-  }
-  brutYield(lostMonthes, monthlyMortgage, monthlyRent, price){
+  calculateYield(basePrice, lostMonthes, monthlyExpenses, monthlyMortgage, monthlyRent, repairCosts){
+    // CLEAN INPUTS to numbers,
+    // if not numbers intput box should light up warning user.
+    let annualExpenses = (monthlyExpenses + monthlyMortgage) * 12;
     let annualRent = (monthlyRent * 12) - (monthlyRent * lostMonthes);
-    let brut = annualRent / price;
-    this.cashFlow(annualRent, monthlyMortgage);
-    return brut;
-  }
-  netYield(lostMonthes, monthlyExpenses, monthlyMortgage, monthlyRent, price){
-    let net = this.brutYield(lostMonthes, monthlyMortgage, monthlyRent, price) * price - (monthlyExpenses * 12) / price;
-    return net;
-  }
-  cashFlow(annual, mortgage){
-    let monthlyCash = annual / 12 - mortgage;
-    return monthlyCash;
+    const notaryFee = 0.09;
+    let totalPrice =  basePrice + (basePrice * notaryFee) + repairCosts;
+    let brutYield = (annualRent / totalPrice) * 100;
+    let netYield = ((annualRent - annualExpenses) / totalPrice) * 100;
+    let monthlyCashFlow = (annualRent - annualExpenses) / 12;
+    this.setState({
+      brut: brutYield,
+      net: netYield,
+      cashFlow: monthlyCashFlow,
+      data:[brutYield,netYield,monthlyCashFlow],
+    });
   }
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
     });
   }
+
   render() {
+    console.log('brut', this.state.brut);
+    console.log('net', this.state.net);
+    console.log('cash', this.state.cashFlow);
+    console.log('data', this.state.data);
+
     return(
       <div className="Calcul">
         <div className="field is-grouped">
           <div className="control">
-            <label className="label">Variable A</label>
+            <label className="label">Price w/out Fees</label>
             <input
               className="input"
-              name="variable_A"
+              name="basePrice"
               onChange={this.handleChange}
-              placeholder="A"
               type="number"
-              value={this.state.variable_A}
+              value={this.state.basePrice}
             />
           </div>
           <div className="control">
-            <label className="label">Variable B</label>
+            <label className="label">Initial Repair Costs</label>
             <input
               className="input"
-              name="variable_B"
+              name="repairCosts"
               onChange={this.handleChange}
-              placeholder="B"
               type="number"
-              value={this.state.variable_B}
+              value={this.state.repairCosts}
             />
+          </div>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <label className="label">Monthly Expenses</label>
+            <input
+              className="input"
+              name="monthlyExpenses"
+              onChange={this.handleChange}
+              type="number"
+              value={this.state.monthlyExpenses}
+            />
+          </div>
+          <div className="control">
+            <label className="label">Monthly Mortgage</label>
+            <input
+              className="input"
+              name="monthlyMortgage"
+              onChange={this.handleChange}
+              type="number"
+              value={this.state.monthlyMortgage}
+            />
+          </div>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <label className="label">Monthly Rent</label>
+            <input
+              className="input"
+              name="monthlyRent"
+              onChange={this.handleChange}
+              type="number"
+              value={this.state.monthlyRent}
+            />
+          </div>
+          <div className="control">
+            <label className="label">Vacant Monthes</label>
+            <input
+              className="input"
+              name="lostMonthes"
+              onChange={this.handleChange}
+              type="number"
+              value={this.state.lostMonthes}
+            />
+          </div>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <div className="answer">
+              <label className="label">Brut</label>
+              {this.state.brut}
+            </div>
           </div>
           <div className="control">
             <div className="answer">
-              <label className="label">Result</label>
-              {this.state.result}
+              <label className="label">Net</label>
+              {this.state.net}
+            </div>
+          </div><div className="control">
+            <div className="answer">
+              <label className="label">Monthly Cash Flow</label>
+              {this.state.cashFlow}
             </div>
           </div>
         </div>
 
         <div className="field">
           <div className="control">
-            <button className="button is-link" onClick={(e) => {e.preventDefault(); this.addition(this.state.variable_A, this.state.variable_B);}}>Submit</button>
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Extra Stuff</label>
-        </div>
-
-        <div className="field">
-          <label className="label">Variable List</label>
-          <div className="control">
-            <div className="select">
-              <select>
-                <option>List Item A</option>
-                <option>List Item B</option>
-                <option>List Item C</option>
-              </select>
-            </div>
-          </div>
-        </div>
-
-        <div className="field">
-          <div className="control">
-            <label className="checkbox">
-              <input type="checkbox"/>
-              I agree to the <a href="">terms and conditions</a>
-            </label>
-          </div>
-        </div>
-
-        <div className="field">
-          <div className="control">
-            <label className="radio">
-              <input className="radio" name="question"/>
-              Yes
-            </label>
-            <label className="radio">
-              <input type="radio" name="question"/>
-              No
-            </label>
-          </div>
-        </div>
-
-        <div className="field is-grouped">
-          <div className="control">
-            <button className="button is-link">Submit</button>
-          </div>
-          <div className="control">
-            <button className="button is-text">Cancel</button>
+            <button className="button is-link" onClick={(e) => {e.preventDefault(); this.calculateYield(this.state.basePrice, this.state.lostMonthes, this.state.monthlyExpenses, this.state.monthlyMortgage, this.state.monthlyRent, this.state.repairCosts);}}>Submit</button>
           </div>
         </div>
       </div>
