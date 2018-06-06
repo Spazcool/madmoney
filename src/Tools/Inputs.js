6980f483ada8de93285194a017b70c503d2b7135
Spazcool <douglas.wrightiii@gmail.com>
2018-06-06 10:59:53 +0800

    tooltip moving now

--- src/Tools/Inputs.js
+++ src/Tools/Inputs.js
@@ -5,83 +5,76 @@ import 'bulma-extensions/bulma-switch/dist/bulma-switch.min.css';
 import React, { Component } from 'react';
 
 class Inputs extends Component {
-  // Find output DOM associated to the DOM element passed as parameter
-  findOutputForSlider( element ) {
-   var idVal = element.id;
-   let outputs = document.getElementsByTagName( 'output' );
-   for( var i = 0; i < outputs.length; i++ ) {
-     if ( outputs[ i ].htmlFor == idVal )
-       return outputs[ i ];
-   }
-}
-//
-// function getSliderOutputPosition( slider ) {
-//   // Update output position
-//   var newPlace,
-//       minValue;
-//
-//   var style = window.getComputedStyle( slider, null );
-//   // Measure width of range input
-//   sliderWidth = parseInt( style.getPropertyValue( 'width' ), 10 );
-//
-//   // Figure out placement percentage between left and right of input
-//   if ( !slider.getAttribute( 'min' ) ) {
-//     minValue = 0;
-//   } else {
-//     minValue = slider.getAttribute( 'min' );
-//   }
-//   var newPoint = ( slider.value - minValue ) / ( slider.getAttribute( 'max' ) - minValue );
-//
-//   // Prevent bubble from going beyond left or right (unsupported browsers)
-//   if ( newPoint < 0 ) {
-//     newPlace = 0;
-//   } else if ( newPoint > 1 ) {
-//     newPlace = sliderWidth;
-//   } else {
-//     newPlace = sliderWidth * newPoint;
-//   }
-//
-//   return {
-//     'position': newPlace + 'px'
-//   }
-// }
+  constructor() {
+    super();
+    this.findOutputForSlider = this.findOutputForSlider.bind(this);
+    this.getSliderOutputPosition = this.getSliderOutputPosition.bind(this);
+    this.handleSlide = this.handleSlide.bind(this);
+  }
+
+  findOutputForSlider(element) {
+    let idVal = element.id;
+    let outputs = document.getElementsByTagName('output');
+    for(let i = 0; i < outputs.length; i++){
+      if (outputs[i].htmlFor == idVal){
+        return outputs[i];
+      }
+    }
+  }
+
+  getSliderOutputPosition(slider) {
+    let newPlace,
+        minValue;
+    let style = window.getComputedStyle(slider, null);
+    let sliderWidth = parseInt( style.getPropertyValue('width'), 10);
+    // Figure out placement percentage between left and right of input
+    if (!slider.getAttribute('min')) {
+      minValue = 0;
+    } else {
+      minValue = slider.getAttribute('min');
+    }
+
+    let newPoint = (slider.value - minValue) / (slider.getAttribute('max') - minValue);
+    // Prevent bubble from going beyond left or right (unsupported browsers)
+    if (newPoint < 0) {
+      newPlace = 0;
+    } else if (newPoint > 1) {
+      newPlace = sliderWidth;
+    } else {
+      newPlace = sliderWidth * newPoint;
+    }
+
+    return {
+      'position': newPlace + 'px'
+    }
+  }
 
   handleSlide(e) {
-    // console.log(e);
-    // Get all document sliders
-    const sliders = document.querySelectorAll( 'input[type="range"].slider' );
-    console.log('SLIDERS', sliders);
+    let slider = document.querySelector('input[type="range"].slider');
+    let output = this.findOutputForSlider(slider);
 
-    [].forEach.call( sliders, function ( slider ) {
+    if (output){
+      if (slider.classList.contains('has-output-tooltip')){
+        // Get new output position
+        let newPosition = this.getSliderOutputPosition(slider);
+        // Set output position
+        output.style['left'] = newPosition.position;
+      }
 
-      const output = this.findOutputForSlider( slider );
-      console.log('OUTPUT', output);
-})
-      // if ( output ) {
-      //   if ( slider.classList.contains( 'has-output-tooltip' ) ) {
+     // Add event listener to update output when slider value change
+      // slider.addEventListener( 'input', function( e ) {
+      //   if ( e.target.classList.contains( 'has-output-tooltip' ) ) {
       //     // Get new output position
-      //     const newPosition = getSliderOutputPosition( slider );
-      //
+      //     let newPosition = this.getSliderOutputPosition( e.target );
       //     // Set output position
       //     output.style[ 'left' ] = newPosition.position;
       //   }
-
-    //     // Add event listener to update output when slider value change
-    //     slider.addEventListener( 'input', function( event ) {
-    //       if ( event.target.classList.contains( 'has-output-tooltip' ) ) {
-    //         // Get new output position
-    //         var newPosition = getSliderOutputPosition( event.target );
-    //
-    //         // Set output position
-    //         output.style[ 'left' ] = newPosition.position;
-    //       }
-    //
-    //       // Update output with slider value
-    //       output.value = event.target.value;
-    //     } );
-    //   }
-    // } );
+      //   // Update output with slider value
+      //   output.value = e.target.value;
+      // } );
+    }
   }
+
   render(){
 
     let switcher =
