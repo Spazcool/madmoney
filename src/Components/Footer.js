9740f041281c94e582aa6687945f1474da461332
Spazcool <douglas.wrightiii@gmail.com>
2018-06-07 12:22:40 +0800

    add share to fb and twt links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,11 +5,11 @@ import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook';
 import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
 import fontawesome from '@fortawesome/fontawesome';
 import React, { Component } from 'react';
-import solid, {faBook, faCode, faHeart, faRss, faToolbox} from '@fortawesome/fontawesome-free-solid';
+import solid, {faBook, faCode, faEnvelope, faHeart, faRss, faToolbox} from '@fortawesome/fontawesome-free-solid';
 
 class Footer extends Component {
   render() {
-    fontawesome.library.add(solid, faBook, faCode, faFacebook, faHeart, faRss, faToolbox, faTwitter);
+    fontawesome.library.add(solid, faBook, faCode, faEnvelope, faFacebook, faHeart, faRss, faToolbox, faTwitter);
 
     return (
       <footer className="footer">
@@ -23,14 +23,15 @@ class Footer extends Component {
                 <p><a href="/blogs"><span className="icon"><i className="fas fa-rss"></i></span>Archive</a></p>
               </div>
               <div className="column">
-                <p className="title is-5">Socialize</p>
-                <p><a href="http://www.facebook.com"><span className="icon"><i className="fab fa-facebook"></i></span>Facebook</a></p>
-                <p><a href="http://www.twitter.com"><span className="icon"><i className="fab fa-twitter"></i></span>Twitter</a></p>
+                <p className="title is-5">Share</p>
+                <p><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A//www.madmoney.com"><span className="icon"><i className="fab fa-facebook"></i></span>Share</a></p>
+                <p><a href="https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Fwww.madmoney.com%2F&ref_src=twsrc%5Etfw&text=Check%20this%20out&tw_p=tweetbutton&url=http%3A%2F%2Fwww.madmoney.com%2F&via=madmoney"><span className="icon"><i className="fab fa-twitter"></i></span>Tweet</a></p>
               </div>
               <div className="column">
-                <p className="title is-5">Checkout</p>
-                <p><a href="http://www.biggerpockets.com">BiggerPockets</a></p>
-                <p><a href="http://www.leboncoin.com">Leboncoin</a></p>
+                <p className="title is-5">Contact</p>
+                <p><a href="http://www.facebook.com/madmoney"><span className="icon"><i className="fab fa-facebook"></i></span>facebook/madmoney</a></p>
+                <p><a href="http://www.twitter.com/madmoney"><span className="icon"><i className="fab fa-twitter"></i></span>@madmoney</a></p>
+                <p><a href="mailto:madmoney@live.fr?subject=I saw your site..."><span className="icon"><i className="fas fa-envelope"></i></span>madmoney@live.fr</a></p>
               </div>
               <div className="column">
                 <p>
