f51b1df827323f573e888a2b7d5f944dd4ea0ace
Spazcool <douglas.wrightiii@gmail.com>
2018-06-15 22:02:05 +0800

    restructure onload function calls

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -9,10 +9,6 @@ import Share from './Share';
 import solid, {faSpinner} from '@fortawesome/fontawesome-free-solid';
 
 class Section extends Component {
-  constructor(props) {
-     super(props);
-     // this.handleImgs = this.handleImgs.bind(this);
-  }
 
   handleImgs(imgs) {
     let _multiple;
@@ -55,10 +51,7 @@ class Section extends Component {
   render() {
     fontawesome.library.add(solid, faSpinner);
 
-    let filtered;
-    let home;
-    let loaded;
-    let mission;
+    let content;
     let loading =
       <section className="section">
         <div className="container">
@@ -69,61 +62,65 @@ class Section extends Component {
       </section>;
 
     if(this.props.loaded){
-      // SHOW SINGLE MATCHING SECTION FOR "/URL/PATH/TO/SECTION"
-      filtered =
-        this.props.data.filter(({fields}, index) =>
-          fields.path === this.props.routing.match.url).map(({fields}, index) =>
-            <section className="section" key={fields.title + index}>
-              <div className="container">
-                <a href={fields.path}><h1 className="title is -1">{fields.title}</h1></a>
-                <h6 className="subtitle">{this.prettyDate(fields.date)}</h6>
-                <p dangerouslySetInnerHTML={this.interpretHTML(marked(fields.description ? fields.description : fields.content))}/>
-                {fields.download ? <a href={fields.download}>Download</a> : ''}
-                <br/>
-                <Share
-                  media={fields.content}
-                  source={fields.path}
-                  title={fields.title}
-                  url={fields.path}
-                />
-              </div>
-            </section>);
-      // SHOW THE 5 MOST RECENT ARTICLES FOR "/"
-      home =
-        this.props.data.slice(0, 5).map(({fields}, index) =>
-          <section className="section" key={fields.title + index}>
-            <div className="container">
-              <a href={fields.path}><h1 className="title is -1">{fields.title}</h1></a>
-              <h6 className="subtitle">{this.prettyDate(fields.date)}</h6>
-              <p dangerouslySetInnerHTML={this.interpretHTML(marked(fields.content))}/>
-              <br/>
-              <Share
-                media={fields.content}
-                source={fields.path}
-                title={fields.title}
-                url={fields.path}
-              />
-              <div className="is-divider"></div>
-            </div>
-          </section>);
-      // SHOW THE 1st || OLDEST ARTICLE AS STANDALONE MISSON STATEMENT
-      mission =
-        this.props.data.filter(({fields}, index) =>
-          index === this.props.data.length - 1).map(({fields}, index) =>
-            <section className="section" key={fields.title + index}>
-              <div className="container">
-                <h1 className="title is -1">{fields.title}</h1>
-                <h6 className="subtitle">{this.prettyDate(fields.date)}</h6>
-                <p dangerouslySetInnerHTML={this.interpretHTML(marked(fields.content))}/>
-              </div>
-            </section>);
-
-      loaded = this.props.routing.match.url === '/' ? (this.props.isMission ? mission : home) : filtered;
+      if(this.props.routing.match.url === '/'){
+        if(this.props.isMission){
+          // SHOW THE 1st || OLDEST ARTICLE AS STANDALONE MISSON STATEMENT
+          content =
+            this.props.data.filter(({fields}, index) =>
+              index === this.props.data.length - 1).map(({fields}, index) =>
+                <section className="section" key={fields.title + index}>
+                  <div className="container">
+                    <h1 className="title is -1">{fields.title}</h1>
+                    <h6 className="subtitle">{this.prettyDate(fields.date)}</h6>
+                    <p dangerouslySetInnerHTML={this.interpretHTML(marked(fields.content))}/>
+                  </div>
+                </section>);
+        }else{
+          // SHOW THE 5 MOST RECENT ARTICLES FOR "/"
+          content =
+            this.props.data.slice(0, 5).map(({fields}, index) =>
+              <section className="section" key={fields.title + index}>
+                <div className="container">
+                  <a href={fields.path}><h1 className="title is -1">{fields.title}</h1></a>
+                  <h6 className="subtitle">{this.prettyDate(fields.date)}</h6>
+                  <p dangerouslySetInnerHTML={this.interpretHTML(marked(fields.content))}/>
+                  <br/>
+                  <Share
+                    media={fields.content}
+                    source={fields.path}
+                    title={fields.title}
+                    url={fields.path}
+                  />
+                  <div className="is-divider"></div>
+                </div>
+              </section>);
+        }
+      }else{
+        // SHOW SINGLE MATCHING SECTION FOR "/URL/PATH/TO/SECTION"
+        content =
+          this.props.data.filter(({fields}, index) =>
+            fields.path === this.props.routing.match.url).map(({fields}, index) =>
+              <section className="section" key={fields.title + index}>
+                <div className="container">
+                  <a href={fields.path}><h1 className="title is -1">{fields.title}</h1></a>
+                  <h6 className="subtitle">{this.prettyDate(fields.date)}</h6>
+                  <p dangerouslySetInnerHTML={this.interpretHTML(marked(fields.description ? fields.description : fields.content))}/>
+                  {fields.download ? <a href={fields.download}>Download</a> : ''}
+                  <br/>
+                  <Share
+                    media={fields.content}
+                    source={fields.path}
+                    title={fields.title}
+                    url={fields.path}
+                  />
+                </div>
+              </section>);
+      }
     }
 
     return (
       <div>
-        {this.props.loaded ? loaded : loading}
+        {this.props.loaded ? content : loading}
       </div>
     );
   }
